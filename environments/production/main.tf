31c41f936f20f46721213bcb354eb23b6321e742
jwr <jwr@jwr.io>
2025-01-12 15:08:20 -0500

    add ec2 server

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -2,6 +2,7 @@ module "ecs_with_codebuild" {
   source           = "../../modules/aws/ecs_with_codebuild"
   environment_name = "cdd"
   on_prem_ip_address = "198.54.135.52/32"
+  launch_production_ec2_instance = true
 }
 
 output "web_to_pdf_hostname" {
--- modules/aws/ecs_with_codebuild/security_groups.tf
+++ modules/aws/ecs_with_codebuild/security_groups.tf
@@ -16,7 +16,15 @@ resource "aws_vpc_security_group_ingress_rule" "web_to_pdf_load_balancer" {
         ip_protocol = "tcp"
         from_port = 443
         to_port = 443
-        description = "Allow HTTPS to the web-to-pdf load balancer"
+        description = "Allow HTTPS from on-prem to the load balancer"
+    },
+    {
+        index_name = "https_from_ec2_servers"
+        referenced_security_group_id = aws_security_group.ec2_servers.id
+        ip_protocol = "tcp"
+        from_port = 443
+        to_port = 443
+        description = "Allow HTTPS from EC2 servers to the load balancer"
     }
   ] : rule.index_name => rule }
   cidr_ipv4 = lookup(each.value, "cidr_ipv4", null)
@@ -36,7 +44,7 @@ resource "aws_vpc_security_group_egress_rule" "web_to_pdf_load_balancer" {
         ip_protocol = "tcp"
         from_port = 443
         to_port = 443
-        description = "Allow HTTPS to the web-to-pdf load balancer"
+        description = "Allow HTTPS from the load balancer to the containers"
     }
   ] : rule.index_name => rule }
   cidr_ipv4 = lookup(each.value, "cidr_ipv4", null)
@@ -66,7 +74,7 @@ resource "aws_vpc_security_group_ingress_rule" "web_to_pdf_containers" {
         ip_protocol = "tcp"
         from_port = 443
         to_port = 443
-        description = "Allow HTTPS from the web-to-pdf load balancer"
+        description = "Allow HTTPS from the load balancer to the containers"
     }
   ] : rule.index_name => rule }
   cidr_ipv4 = lookup(each.value, "cidr_ipv4", null)
@@ -116,7 +124,15 @@ resource "aws_vpc_security_group_ingress_rule" "svg_to_pdf_load_balancer" {
         ip_protocol = "tcp"
         from_port = 443
         to_port = 443
-        description = "Allow HTTPS to the svg-to-pdf load balancer"
+        description = "Allow HTTPS from on-prem to the load balancer"
+    },
+    {
+        index_name = "https_from_ec2_servers"
+        referenced_security_group_id = aws_security_group.ec2_servers.id
+        ip_protocol = "tcp"
+        from_port = 443
+        to_port = 443
+        description = "Allow HTTPS from EC2 servers to the load balancer"
     }
   ] : rule.index_name => rule }
   cidr_ipv4 = lookup(each.value, "cidr_ipv4", null)
@@ -136,7 +152,7 @@ resource "aws_vpc_security_group_egress_rule" "svg_to_pdf_load_balancer" {
         ip_protocol = "tcp"
         from_port = 443
         to_port = 443
-        description = "Allow HTTPS to the svg-to-pdf load balancer"
+        description = "Allow HTTPS from the load balancer to the containers"
     }
   ] : rule.index_name => rule }
   cidr_ipv4 = lookup(each.value, "cidr_ipv4", null)
@@ -166,7 +182,7 @@ resource "aws_vpc_security_group_ingress_rule" "svg_to_pdf_containers" {
         ip_protocol = "tcp"
         from_port = 443
         to_port = 443
-        description = "Allow HTTPS from the svg-to-pdf load balancer"
+        description = "Allow HTTPS from the load balancer to the containers"
     }
   ] : rule.index_name => rule }
   cidr_ipv4 = lookup(each.value, "cidr_ipv4", null)
@@ -198,4 +214,41 @@ resource "aws_vpc_security_group_egress_rule" "svg_to_pdf_containers" {
   description = lookup(each.value, "description", null)
 }
 
+resource "aws_security_group" "ec2_servers" {
+  name        = "${var.environment_name}-ec2-servers"
+  description = "Used by EC2 servers which connect to the ECS services built in this Terraform module."
+  vpc_id      = aws_vpc.default.id
+
+  tags = {
+    Name = "${var.environment_name}-ec2-servers"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_servers" {
+  for_each = { for rule in [
+    {
+        index_name = "https_to_web_to_pdf"
+        referenced_security_group_id = aws_security_group.web_to_pdf_load_balancer.id
+        ip_protocol = "tcp"
+        from_port = 443
+        to_port = 443
+        description = "Allow EC2 servers to connect to the web-to-pdf load balancer."
+    },
+    {
+        index_name = "https_to_svg_to_pdf"
+        referenced_security_group_id = aws_security_group.svg_to_pdf_load_balancer.id
+        ip_protocol = "tcp"
+        from_port = 443
+        to_port = 443
+        description = "Allow EC2 servers to connect to the svg-to-pdf load balancer."
+    }
+  ] : rule.index_name => rule }
+  cidr_ipv4 = lookup(each.value, "cidr_ipv4", null)
+  referenced_security_group_id = lookup(each.value, "referenced_security_group_id", null)
+  ip_protocol = lookup(each.value, "ip_protocol", null)
+  from_port = lookup(each.value, "from_port", null)
+  to_port = lookup(each.value, "to_port", null)
+  security_group_id = aws_security_group.ec2_servers.id
+  description = lookup(each.value, "description", null)
+}
 
