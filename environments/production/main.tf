433a6a878c4e98caa66f8966c71248fc0eec3af6
jwr <jwr@jwr.io>
2025-01-12 16:08:00 -0500

    clean up main.tf

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,8 +1,9 @@
 module "ecs_with_codebuild" {
-  source           = "../../modules/aws/ecs_with_codebuild"
-  environment_name = "cdd"
-  on_prem_ip_address = "198.54.135.52/32"
-  launch_production_ec2_instance = true
+  source                         = "../../modules/aws/ecs_with_codebuild"
+  environment_name               = "cdd"
+  on_prem_ip_address             = "1.2.3.4/32" # Replace with your on-prem (laptop) IP addres
+  acm_certificate_arn            = "..."        # Replace with your ACM certificate ARN
+  launch_production_ec2_instance = false        # Set to true to launch EC2 instance to simulate connectivity from within AWS.
 }
 
 output "web_to_pdf_hostname" {
